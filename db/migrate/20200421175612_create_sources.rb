a8b5a1147164d3d271dd8ebdabf45c8887170226
nbaugh1 <nbaugh1@gmail.com>
2020-04-21 14:07:20 -0400

    migrate db

--- db/migrate/20200421175612_create_sources.rb
+++ db/migrate/20200421175612_create_sources.rb
@@ -3,11 +3,11 @@ class CreateSources < ActiveRecord::Migration[6.0]
     create_table :sources do |t|
       t.references :category, null: false, foreign_key: true
       t.string :name
-      t.string :id
+      t.string :source_id
       t.string :description
       t.string :url
       t.string :category
-      t.sting :language
+      t.string :language
       t.string :country
 
       t.timestamps
---
+++ db/schema.rb
@@ -0,0 +1,61 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_21_175739) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.bigint "source_id", null: false
+    t.string "title"
+    t.string "description"
+    t.string "url"
+    t.string "published"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["source_id"], name: "index_articles_on_source_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "sources", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.string "name"
+    t.string "source_id"
+    t.string "description"
+    t.string "url"
+    t.string "category"
+    t.string "language"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_sources_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "articles", "sources"
+  add_foreign_key "categories", "users"
+  add_foreign_key "sources", "categories"
+end
